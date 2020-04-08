b0b54b56c5d5c7cddc56fbbe3222c0fa71a047bd
IJuser <khrisys@laposte.net>
2020-04-08 18:22:42 +0200

    Inclusion de Srong Admin pour se servir de Actuator dont on avait dejà eu l'utilité pour declancher une requete vers les MS pour les forcer à prendre en compte des changements dans les MS sans qu'ils soient redeployés.
    Actuator est une veritable API REST qui dispose d'un grand nb d'url pour pouvoir connaitre l'etat d'un MS. POur faciliter toutes ces requetes, on va utiliser Spring Admin qui permet de visualiser la plupart de ces reuqtes dans des tableaux etschemas via une IHM.
    
    Signed-off-by: IJuser <khrisys@laposte.net>

--- clientui/src/main/java/com/clientui/exceptions/CommandeNotFoundException.java
+++ clientui/src/main/java/com/clientui/exceptions/CommandeNotFoundException.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class CommandeNotFoundException extends RuntimeException {
-
-
+    
     public CommandeNotFoundException(String message) {
         super(message);
     }
